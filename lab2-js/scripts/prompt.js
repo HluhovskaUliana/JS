1d004dbe5e61a79fdd996f543ba4ba5bd5c311d2
HluhovskaUliana <uglugovska@gmail.com>
2025-11-17 21:33:41 +0200

    1

---
+++ lab2-js/scripts/prompt.js
@@ -0,0 +1,18 @@
+﻿// ----- Об'єкту myTxt присвоїти значення селекторів 'h1.n2'
+var myTxt = document.querySelector('h1.n2');
+
+// ----- Об'єкту myTxt присвоїти значення селекторів '.myclass'
+var myFont = document.querySelectorAll('.myclass');
+
+for (var i = 0; i < myFont.length; i++) {
+    myFont[i].style.fontSize = '35px';
+}
+
+myTxt.innerHTML = 'Привіт, студенте!';
+
+var person = prompt('Як Вас звати');
+if (person != null) {
+    myTxt.innerHTML = "Я вітаю Вас " + person + "!" + "<br>" + " ";
+}
+
+myTxt.style.color = 'red';
