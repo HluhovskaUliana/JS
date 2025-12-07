d92f01c22a3420210670e4ebe581ea0c35d1a0b0
HluhovskaUliana <uglugovska@gmail.com>
2025-12-07 13:55:56 +0200

    add lab9

---
+++ lab9-js/result.php
@@ -0,0 +1,16 @@
+﻿<html>
+<head><title>Результат замовлення</title></head>
+<body>
+<?php
+$product = $_POST["product"];
+$material = $_POST["material"];
+$quantity = $_POST["quantity"];
+
+echo "<h2>Ваше замовлення прийнято</h2>";
+echo "Замовлено виріб: $product <br>";
+echo "Матеріал: $material <br>";
+echo "Кількість: $quantity <br>";
+?>
+<p><a href="order.html">Повернутися до форми</a></p>
+</body>
+</html>
